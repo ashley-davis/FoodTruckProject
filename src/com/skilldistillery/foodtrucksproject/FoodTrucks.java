652d84bef6213103c6ced37592f3d65a493e4ce2
Ashley <yeleomg@Ashleys-MacBook-Air.local>
2020-09-05 14:09:02 -0600

    user prompt

---
+++ src/com/skilldistillery/foodtrucksproject/FoodTrucks.java
@@ -0,0 +1,11 @@
+package com.skilldistillery.foodtrucksproject;
+
+public class FoodTrucks {
+//Fields for unique ID, food truck name, and numeric rating
+	public double uniqueId;
+	public String truckName;
+	public String foodType;
+	public int rating;
+
+}	
+
